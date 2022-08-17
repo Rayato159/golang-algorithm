96730265d790d31e9c87891e93cd76b0051b1b85
Rayato159 <rayato159@gmail.com>
2022-08-18 00:35:49 +0700

    -fix :bug: :Queue false size was fixed.

--- Queue/Queue.go
+++ Queue/Queue.go
@@ -4,41 +4,54 @@ import "fmt"
 
 type Queue struct {
 	Data  []int
-	First int
+	Size  int
+	Front int
 	Rear  int
 }
 
 func NewQueue(size int) *Queue {
 	return &Queue{
-		Data:  make([]int, size+1),
-		First: 0,
+		Data:  make([]int, size+2),
+		Size:  size + 1,
+		Front: 0,
 		Rear:  0,
 	}
 }
 
 func (q *Queue) Enqueue(x int) {
-	if (q.First+1)%(cap(q.Data)-1) == q.Rear {
-		fmt.Println("error, queue is full.")
+	if (q.Rear+1)%q.Size == q.Front {
+		fmt.Println("error, queue is full")
 	} else {
-		q.Rear = (q.Rear + 1) % (cap(q.Data) - 1)
+		q.Rear = (q.Rear + 1) % q.Size
 		q.Data[q.Rear] = x
-		fmt.Println(q.Data, q.First, q.Rear)
+		fmt.Println(q.Data, q.Front, q.Rear)
 	}
 }
 
-func (q *Queue) Dequeue() {
-	if q.First == q.Rear {
-		fmt.Println("error, queue is empty.")
+func (q *Queue) Dequeue() int {
+	var x int
+	if q.Front == q.Rear {
+		fmt.Println("error, queue is empty")
 	} else {
-		q.First = (q.First + 1) % (cap(q.Data) - 1)
-		q.Data[q.First] = 0
-		fmt.Println(q.Data, q.First, q.Rear)
+		q.Front = (q.Front + 1) % q.Size
+		x = q.Data[q.Front]
+		q.Data[q.Front] = 0
+		fmt.Println(q.Data, q.Front, q.Rear)
 	}
+	return x
 }
 
 func main() {
 	q := NewQueue(5)
-	q.Enqueue(1)
+	fmt.Println(q)
+	q.Enqueue(10)
+	q.Enqueue(20)
+	q.Enqueue(30)
+	q.Enqueue(40)
+	q.Enqueue(50)
+	q.Dequeue()
+	q.Dequeue()
+	q.Dequeue()
 	q.Dequeue()
 	q.Dequeue()
 }
