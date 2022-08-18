139e64cd0fadb68e1d880552c7bd18b35790d06e
Rayato159 <rayato159@gmail.com>
2022-08-18 20:37:18 +0700

    -fix :bug: :Peek false was fixed.

--- Stack/Stack.go
+++ Stack/Stack.go
@@ -18,7 +18,7 @@ func NewStack(size int) *Stack {
 
 func (s *Stack) Push(x int) {
 	if s.Top == s.Size-1 {
-		fmt.Println("error, stack overflow")
+		fmt.Println("error, stackoverflow")
 	} else {
 		s.Top++
 		s.Data[s.Top] = x
@@ -28,7 +28,7 @@ func (s *Stack) Push(x int) {
 func (s *Stack) Pop() int {
 	var x int
 	if s.Top == -1 {
-		fmt.Println("error, stack is empty")
+		fmt.Println("error, underflow")
 	} else {
 		x = s.Data[s.Top]
 		s.Data[s.Top] = 0
@@ -37,12 +37,12 @@ func (s *Stack) Pop() int {
 	return x
 }
 
-func (s *Stack) Peek(i int) int {
+func (s *Stack) Peek() int {
 	var x int
-	if s.Data[s.Top+i-1] <= 0 {
+	if s.Data[s.Top] == 0 {
 		fmt.Println("error, invalid position")
 	} else {
-		x = s.Data[s.Top+i-1]
+		x = s.Data[s.Top]
 	}
 	return x
 }
@@ -52,6 +52,11 @@ func main() {
 	s.Push(10)
 	s.Push(20)
 	s.Push(30)
+	s.Push(40)
+	s.Push(50)
+	s.Pop()
+	s.Pop()
+	s.Pop()
+	fmt.Println(s.Peek())
 	fmt.Println(s.Data, s.Top)
-	fmt.Println(s.Peek(1))
 }
